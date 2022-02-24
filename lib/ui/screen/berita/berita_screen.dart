fc20a760000f016198e92418d6eabe3ee929ebe0
Irfan Maulana <irfan.resimen2018@gmail.com>
2022-02-24 23:17:56 +0700

    add berita

---
+++ lib/ui/screen/berita/berita_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BeritaScreen extends StatefulWidget {
+  const BeritaScreen({Key? key}) : super(key: key);
+
+  @override
+  _BeritaScreenState createState() => _BeritaScreenState();
+}
+
+class _BeritaScreenState extends State<BeritaScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
